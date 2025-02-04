0f153fb8604fd2151a6a8dbcc2e77873c3324c24
jimmyMithamo <j1mithamo@gmail.com>
2025-02-04 22:50:20 +0300

    csrf tokens

--- deeper_attendance/deeper_attendance/settings.py
+++ deeper_attendance/deeper_attendance/settings.py
@@ -127,3 +127,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://www.tutumekenya.co.ke', 'https://tutumekenya.co.ke']
