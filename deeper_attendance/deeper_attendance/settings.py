f5363a347641438f1fd3638a22da966a7fe85f18
jimmyMithamo <j1mithamo@gmail.com>
2025-02-05 14:08:14 +0300

    settings file adjust

--- deeper_attendance/deeper_attendance/settings.py
+++ deeper_attendance/deeper_attendance/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
